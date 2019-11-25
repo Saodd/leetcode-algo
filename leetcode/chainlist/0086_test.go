6e9368a8028867c5175521b5664cd960ab681463
Lewin Lan <lewinlan@dingtalk.com>
2019-11-25 10:26:40 +0800

    LeetCode 86

---
+++ leetcode/chainlist/0086_test.go
@@ -0,0 +1,32 @@
+package chainlist
+
+import (
+	"github.com/Saodd/leetcode-algo/common"
+	"reflect"
+	"testing"
+)
+
+func Test_partition(t *testing.T) {
+	type args struct {
+		head *common.ListNode
+		x    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *common.ListNode
+	}{
+		{
+			name: "示例1",
+			args: args{common.CreateListInt([]int{1, 4, 3, 2, 5, 2}), 3},
+			want: common.CreateListInt([]int{1, 2, 2, 4, 3, 5}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := partition(tt.args.head, tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("partition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
