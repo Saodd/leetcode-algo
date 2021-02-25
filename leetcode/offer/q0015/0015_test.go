3c66fa9928a85d618a5b652563b2d5c98c40507a
Lin Lan <lanlin@meideng.net>
2021-02-25 15:17:50 +0800

    offer 15

---
+++ leetcode/offer/q0015/0015_test.go
@@ -0,0 +1,37 @@
+package q0015
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "官方用例1",
+			args: args{0b00000000000000000000000000001011},
+			want: 3,
+		},
+		{
+			name: "官方用例2",
+			args: args{0b00000000000000000000000010000000},
+			want: 1,
+		},
+		{
+			name: "官方用例3",
+			args: args{0b11111111111111111111111111111101},
+			want: 31,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
